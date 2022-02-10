487eddf137a7d6d832e5f7642c0914b68f3af9a5
Ismail <forwork.ismail@gmail.com>
2022-02-10 21:44:42 +0430

    Create reader variable for reading user input

---
+++ bmi.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+var reader = bufio.NewReader(os.Stdin)
+
+func main() {
+
+	fmt.Println("BMI Calculator")
+	fmt.Println("--------------------")
+	fmt.Print("Please enter your weight (kg): ")
+	weightInput, _ := reader.ReadString('\n')
+
+	fmt.Print("Please enter your height (m): ")
+	heightInput, _ := reader.ReadString('\n')
+
+	fmt.Print(weightInput)
+	fmt.Print(heightInput)
+
+}
