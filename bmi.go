aa66e509e6e984e8315cd62abf1f37bf394abb38
Ismail <forwork.ismail@gmail.com>
2022-02-12 21:12:49 +0430

    Create printBMI function

--- bmi.go
+++ bmi.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"fmt"
-
 	"github.com/ismailwork/golang-BMI-calculator/info"
 )
 
@@ -14,7 +12,7 @@ func main() {
 
 	bmi := calculateBMI(weight, height)
 
-	fmt.Printf("Your BMI: %.2f", bmi)
+	printBMI(bmi)
 
 }
 
